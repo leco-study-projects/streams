d9ed1d3de381288a42ff453c3ba4a24c39ffb7ac
leco <lcosta.sp.br@gmail.com>
2019-01-07 16:15:56 -0200

    "Create structure for understanding streams"

--- src/test/java/guru/leco/AppTest.java
+++ src/test/java/guru/leco/AppTest.java
@@ -1,20 +1,31 @@
 package guru.leco;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void changeNumberInStream() throws InterruptedException {
+
+        List<Integer> numbers = Arrays.asList(2, 3, 4, 6, 7);
+
+        final Integer[] multiples = {2};
+
+        Stream<Integer> numberStreams = numbers
+                .stream()
+                .map(integer -> integer * multiples[0]);
+
+        Thread.sleep(10000);
+
+        multiples[0] = 3;
+
+        numberStreams.forEach(System.out::println);
     }
+
 }
