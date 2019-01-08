c9e13e44b7808a0c505e117e75bb9e0b23538a90
Leandro da Silva Costa <leandro.edeploy@stelo.com.br>
2019-01-08 10:18:02 -0200

    test group by

--- src/test/java/guru/leco/AppTest.java
+++ src/test/java/guru/leco/AppTest.java
@@ -1,10 +1,13 @@
 package guru.leco;
 
+import guru.leco.domain.User;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Unit test for simple App.
@@ -27,5 +30,41 @@ public class AppTest {
 
         numberStreams.forEach(System.out::println);
     }
+
+    @Test
+    public void streamsGroupBy() {
+
+        List<User> users = Arrays.asList(new User("Leandro", 26), new User("Marcos", 19), new User("Juliana", 26));
+
+        users.stream().collect(groupingBy(User::getAge)).forEach((integer, items) -> {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Quantidade : ");
+            builder.append(items.size());
+            builder.append(" Age: ");
+            builder.append(integer);
+            System.out.println(builder.toString());
+        });
+    }
+
+    @Test
+    public void streamFilter() {
+        List<User> users = Arrays.asList(new User("Leandro", 26), new User("Marcos", 19), new User("Juliana", 26));
+        users.stream()
+                .filter(user -> user.getAge() > 20)
+                .forEach(user -> {
+                    System.out.println(user.getName());
+                });
+    }
+
+    @Test
+    public void streamParallelStream() {
+        List<User> users = Arrays.asList(new User("Leandro", 26), new User("Marcos", 19), new User("Juliana", 26));
+        users.stream()
+                .filter(user -> user.getAge() > 20)
+                .parallel()
+                .forEach(user -> System.out.println(user.getName()));
+    }
+
+
 
 }
